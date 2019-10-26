89607fb838763b232a7f2df3324e1b51cf633d7b
Guillermo Miralles Campillo <mirallescampilloguillermo@gmail.com>
2019-10-26 16:14:56 +0200

    Added javadocs for readiness and liveness probes

--- src/main/java/com/nfkdata/seed/rest/probes/LivenessProbe.java
+++ src/main/java/com/nfkdata/seed/rest/probes/LivenessProbe.java
@@ -5,9 +5,16 @@ import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
 
+/**
+ * Checking the alive status of the application
+ */
 @Component
-public class LivenessProbe implements HealthIndicator{
+public class LivenessProbe implements HealthIndicator {
 
+	/**
+	 * Called when checking the alive status
+	 * @return Health object with the current status and checks of the app
+	 */
 	@Override
 	public Health health() {
 		return Health.status(Status.UP).build();
--- src/main/java/com/nfkdata/seed/rest/probes/ReadinessProbe.java
+++ src/main/java/com/nfkdata/seed/rest/probes/ReadinessProbe.java
@@ -8,9 +8,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+/**
+ * Checking the readiness status of the application
+ */
 @Endpoint(id = "ready")
 @Component
 public class ReadinessProbe {
+	
+	/**
+	 * Called when checking the readiness status
+	 * @return Health object with the current status and checks of the app
+	 */
 	@ReadOperation
 	public ResponseEntity<Health> health() {
 		return new ResponseEntity<>(Health.status(Status.UP).build(), HttpStatus.OK);
